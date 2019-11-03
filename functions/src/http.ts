8a7ef93074408fb4a615fa4f3a61ff379ba6e3a2
Maicon Andraski <maiconm23@gmail.com>
2019-11-03 10:53:05 -0300

    Testing Background Funcions with shell
    - commented `initializeApp()`
    - Added `service-account.json` to `.gitignore`
    
    Steps to reproduce:
    - generate a service account on firebase console;
    - create a colletion with you use on  your function;
    - move your service-account file to `Functions` folder;
    - run `set GOOGLE_APPLICATION_CREDENTIALS=[path/service-account]`;
    - restart your OS;
    - run `npm run shell`;
    - then run your function (ex.: `createUserRecord({uid: 'foo'})`);
    
    After all these steps, everything should worked fine!

--- functions/src/http.ts
+++ functions/src/http.ts
@@ -1,9 +1,9 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
 
-admin.initializeApp();
+// admin.initializeApp();
 
 export const basicHTTP = functions.https.onRequest((request, response) => {
   const name = request.query.name;
